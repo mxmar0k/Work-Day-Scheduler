88f00c9152cc6bbf53f6e2d6b2f50a3981aed557
Hola Bart <marcoferrusquia@gmail.com>
2023-07-06 21:53:57 -0600

    we added the class add for time, present and future and the local storage retriver

--- script.js
+++ script.js
@@ -13,11 +13,40 @@ $(function () {
   $(".saveBtn").on("click", function () {
     // 3. we got the id of the parent time-block with variable associated with the element-trigger (aka the click on the save button) and then find the closest ancestor element to the time block, so when we click the save button on hour 9, then it logs "hour-9" div id, because we ask for that att. 
     var timeBlockId = $(this).closest(".time-block").attr("id");
-    console.log(timeBlockId)
+    console.log(timeBlockId);
     // 4. then we get the user input via the text area next to the save button by selecting all brothers and sisters of the description class, but refering only to the sibling of the clicked button. 
     var userInput = $(this).siblings(".description").val();
-    // 5. Next we save the user input in the local storage using the time-block id as the key and the user inputs value. Again we used a console log to check what we did with the user input, to check if it logs correctly.
-    console.log(userInput)
+    // 5. Next we save the user input in the local storage using the time-block id as the key and the user inputs valueeee. Again we used a console log to check what we did with the user input, to check if it logs correctly.
+    console.log(userInput);
     localStorage.setItem(timeBlockId, userInput);
+  });
+
+  // 6. nxt we applied the past, present, or future class to each time block, depending on the time
+  /*7. we retrived the current hour with they dayjs and converted it to "h" format, just as a 2 digit number
+  then we selected all the elements with the time block class and retrieved their id, since it is "hour-9" for example, we splited it and got the number as an integer
+  we use a console.log to check if that worked.
+  then we use if statements to check the hours and add the necessary class.
+  */
+  var currentHour = dayjs().format("H");
+  $(".time-block").each(function () {
+    var timeBlockId = $(this).attr("id");
+    var hour = parseInt(timeBlockId.split("-")[1]);
+    console.log(hour);
+    if (hour < currentHour) {
+      $(this).addClass("past");
+    } else if (hour === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
+
+  // 8. next we got the user input from the local storage and we distribute the corresponding saved value to its original place when it was saved. we used the same structure as before and we only got the stored value
+  $(".time-block").each(function () {
+    var timeBlockId = $(this).attr("id");
+    var savedInput = localStorage.getItem(timeBlockId);
+    if (savedInput) {
+      $(this).find(".description").val(savedInput);
+    }
   });
 });
